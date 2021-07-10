dc653806a962c8a86029e3b91465c64d6f31826d
Kyle Sullivan <kylesullivan123@gmail.com>
2021-07-10 11:02:38 -0700

    commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,107 @@
+import React, {useState} from 'react'
+
+const Form = props => {
+    // const [firstName, setFirstName] =useState('')
+    // const [lastName, setLastName] =useState('')
+    // const [email, setEmail] =useState('')
+    // const [password, setPassword] = useState('')
+
+    const [formState, setFormState] = useState({
+        firstName : '',
+        lastName : '',
+        email : '',
+        password : '',
+        confPassword : ''
+    })
+
+    const [validState, setValidState] = useState({
+        firstName: false,
+        lastName: false,
+        email: false,
+        password: false,
+        confPassword: false
+    })
+
+
+    const handleChange = event => {
+        const {name, value} = event.target
+
+        setFormState({
+        ...formState,
+        [name] : value
+        })
+        //setEmail(event.target.value)
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        if(formState.firstName.length < 2){
+            var firstName = true
+        }
+        if(formState.lastName.length < 2){
+            var lastName = true
+        }
+        if(formState.email.length < 5){
+            var email = true
+        }
+        if(formState.password.length < 8){
+            var password = true
+        }
+        if(formState.password !== formState.confPassword){
+            var confPassword = true
+        }
+
+        setValidState({
+            ...validState,
+            firstName,
+            lastName,
+            email,
+            password,
+            confPassword  
+        })
+    }
+    
+
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <p>
+                    First Name:
+                    <input type="text" name="firstName" onChange={handleChange} id="" />
+                    { (validState.firstName) ? <p>First Name must be at least 2 characters</p> : null }
+                </p>
+                <p>
+                    Last Name:
+                    <input type="text" name="lastName" onChange={handleChange} id="" />
+                    { (validState.lastName) ? <p>Last Name must be at least 2 characters</p> : null }
+                </p>
+                <p>
+                    Email:
+                    <input type="text" name="email" onChange={handleChange} id="" />
+                    { (validState.email) ? <p>Your email must be at least 5 characters</p> : null }
+                </p>
+                <p>
+                    Password:
+                    <input type="password" name="password" onChange={handleChange} id="" />
+                    { (validState.password) ? <p>Password must be at least 8 characters</p> : null }
+                </p>
+                <p>
+                    Confirm Password:
+                    <input type="password" name="confPassword" onChange={handleChange} id="" />
+                    { (validState.confPassword) ? <p>Passwords must match</p> : null }
+                </p>
+                <button type="submit">Create User</button>
+            </form>
+            <div>
+                <p>First Name: {formState.firstName}</p>
+                <p>Last Name: {formState.lastName}</p>
+                <p>Email: {formState.email}</p>
+                <p>Password: {formState.password}</p>
+                <p>Confirm Password: {formState.confPassword}</p>
+            </div>
+        </div>
+    )
+    }
+
+export default Form
